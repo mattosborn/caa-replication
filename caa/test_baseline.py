82aeb62cbc228f31a7c9189e73159538ac78c776
Matt Osborn <mattjosborn@gmail.com>
2024-09-07 11:03:16 +0100

    baseline probability test on all behaviours

--- caa/test_baseline.py
+++ caa/test_baseline.py
@@ -1,12 +1,13 @@
+import torch as t
 import os
 from tqdm import tqdm
 from .model import ModelWrapper
 from .utils import load_dataset, behaviours
-
+from statistics import mean
 
 def test_baseline(model_name: str):
-    model = ModelWrapper(model_name, os.getenv('HF_TOKEN'))
-
+    model = ModelWrapper(model_name)
+    
     for behaviour in behaviours:
         test_dataset = load_dataset(f'{behaviour}_test_ab')
 
@@ -20,13 +21,14 @@ def test_baseline(model_name: str):
         for data in tqdm(test_dataset, leave=False):
             prompt = model.tokenize_question(data['question'], '(')
             baseline = model(prompt)
-            baseline_answer = model.tokenizer.decode(
-                baseline.logits[0, -1].argmax())
-            is_matching = baseline_answer == data['answer_matching_behavior'][1]
-            answers.append(is_matching)
+            
+            probabilities = t.softmax(baseline.logits[0, -1], -1)
+            
+            answer_char = data['answer_matching_behavior'][1]
+            behaviour_token = model.tokenizer.convert_tokens_to_ids(answer_char)
+            
+            answers.append(probabilities[behaviour_token].item())
 
-        answers_matching_behaviour = len(
-            [a for a in answers if a == True])
-        result = answers_matching_behaviour / len(answers)
-        print(
-            f'{behaviour}: {answers_matching_behaviour}/{len(answers)} ({result*100:.2f}%)')
+        mean_answers = mean(answers)
+        print(f'{behaviour}: {mean_answers:.4f}')
+        
