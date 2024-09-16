9797cb0e2ecc0a2c4dad39664679f36ad0fa0a31
Matt Osborn <mattjosborn@gmail.com>
2024-09-16 19:16:59 +0100

    test all steering vectors

--- caa/test_steering_vectors.py
+++ caa/test_steering_vectors.py
@@ -1,38 +1,28 @@
 import torch as t
-import os
+import json
 from tqdm import tqdm
-from .model import ModelWrapper
-from .utils import load_dataset
-
+from caa.model import ModelWrapper
+from caa.utils import load_dataset, behaviours
 
 def test_steering_vectors(model_name: str):
-    model = ModelWrapper(model_name, os.getenv('HF_TOKEN'))
+    model = ModelWrapper(model_name)
+    multipliers = [-1, -0.5, 0, 0.5, 1]
 
-    steering_vectors = t.load('data/steering_vectors.pt')
-    baseline_answers = []
-    steered_answers = []
+    steering_results = {}
 
-    test_dataset = load_dataset('refusal_test_ab')
+    for behaviour in behaviours:
+        results = {m: [] for m in multipliers}
+        dataset = load_dataset(f"{behaviour}_test_ab")
+        steering_vectors = t.load(f'data/{behaviour}_steering_vectors.pt')
 
-    for data in test_dataset[:2]:
-        print(f"\n-------------\n\n{data['question']}\n")
+        for data in tqdm(dataset):
+            prompt = model.tokenize_question(data['question'], '(')
+            for m in multipliers:
+                steered_output = model.prompt_with_steering(prompt, steering_dict={13: steering_vectors[13] * m})
+                prob_behaviour = model.calc_behaviour_prob(steered_output.logits, data)
+                results[m].append(prob_behaviour)
 
-        prompt = model.tokenize_question(data['question'], '(')
-        baseline = model(prompt)
+        steering_results[behaviour] = {m: sum(results[m]) / len(results[m]) for m in multipliers}
 
-        indices = baseline.logits[0, -1].topk(5).indices
-        probabilities = t.softmax(baseline.logits[0, -1], -1)
-        print("BASELINE:")
-        for i in range(5):
-            print(
-                f"{i}: {model.tokenizer.decode(indices[i])} ({probabilities[indices[i]]})")
-
-        steered_output = model.prompt_with_steering(
-            prompt, steering_dict={13: steering_vectors[13]})
-
-        indices = steered_output.logits[0, -1].topk(5).indices
-        probabilities = t.softmax(steered_output.logits[0, -1], -1)
-        print("STEERED:")
-        for i in range(5):
-            print(
-                f"{i}: {model.tokenizer.decode(indices[i])} ({probabilities[indices[i]]})")
+    with open(f'results/{model.get_model_name()}_steering_results.json', 'w') as f:
+        json.dump(steering_results, f, indent=2)
