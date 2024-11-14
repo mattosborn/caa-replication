9b1b2fac00ba24a2a5d1551e75ff44bf6e99f512
Matt Osborn <mattjosborn@gmail.com>
2024-11-14 09:43:17 +0000

    fix baseline script

--- caa/test_baseline.py
+++ caa/test_baseline.py
@@ -2,11 +2,14 @@ from tqdm import tqdm
 from .model import ModelWrapper
 from .utils import load_dataset, behaviours
 from statistics import mean
+import json
 
 device = "cuda:0"
 
 def test_baseline(model_name: str):
     model = ModelWrapper(model_name)
+    
+    results = {}
     
     for behaviour in behaviours:
         test_dataset = load_dataset(f'{behaviour}_test_ab')
@@ -19,11 +22,15 @@ def test_baseline(model_name: str):
         answers = []
 
         for data in tqdm(test_dataset, leave=False):
-            prompt = model.tokenize_question(data['question'], '(').to(device)
+            prompt = model.tokenize_question(data['question'], '(').unsqueeze(0).to(device)
             baseline = model(prompt)
-            prob_behaviour = model.calc_behaviour_prob(baseline.logits, data)
+            prob_behaviour = model.calc_behaviour_prob(baseline.logits[0, -1], data)
             answers.append(prob_behaviour)
 
         mean_answers = mean(answers)
         print(f'{behaviour}: {mean_answers:.4f}')
+        results[behaviour] = mean_answers
+    
+    with open(f'results/{model.get_model_name()}_baselines.json', 'w') as f:
+        json.dump(results, f, indent=2)
         
